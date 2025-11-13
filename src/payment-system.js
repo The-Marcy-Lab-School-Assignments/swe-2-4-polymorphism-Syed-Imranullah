b5edc43a452257837409734abad838646fa75cf5
Syed <xayanmay@gmail.com>
2025-11-12 21:19:05 -0500

    working on it

--- src/payment-system.js
+++ src/payment-system.js
@@ -1,20 +1,33 @@
 /* eslint-disable max-classes-per-file */
 
 class Payment {
-
+  constructor(amount, recipient) {
+    this.amount = amount;
+    this.recipient = recipient;
+    this.status = "pending";
+  }
+  process() {
+    this.status = "completed";
+    return `Payment of $${this.amount} to ${this.recipient} completed`;
+  }
+  getDetails() {
+    return `$${this.amount} to ${this.recipient} - Status: ${this.status}`
+  }
 }
 
-class CreditCardPayment {
-
+class CreditCardPayment extends Payment {
+  constructor(amount, recipient, cardNumber) {
+    super(amount, recipient)
+    this.cardNumber = cardNumber
+  }
+  getDetails(){
+    return `${super.getDetails} (Card: ${this.cardNumber})`
+  }
 }
 
-class PayPalPayment {
+class PayPalPayment {}
 
-}
-
-const processPayments = (payments) => {
-
-};
+const processPayments = (payments) => {};
 
 module.exports = {
   Payment,
